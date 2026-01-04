209168588af6ab1273dcdefa1621fea05eee6b16
Mourya1123 <mourya.dv1@gmail.com>
2026-01-04 14:32:09 +0530

    - .equals program.

---
+++ src/main/java/com/project/main/equals.java
@@ -0,0 +1,12 @@
+package com.project.main;
+
+public class equals {
+    public static void main(String[] args) {
+
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println( s1 + " " + s2 ); // Printing s1, s2 values
+        System.out.println(s1.equals(s2)); //   Checking Contents of both String variables
+
+    }
+}
