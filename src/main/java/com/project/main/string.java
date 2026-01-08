deff86f5decb5b2d25b8b19db82581aed9628187
Mourya1123 <mourya.dv1@gmail.com>
2026-01-08 18:32:49 +0530

    My First string

---
+++ src/main/java/com/project/main/string.java
@@ -0,0 +1,15 @@
+package com.project.main;
+
+public class string {
+    public static void main(String[] args){
+        //Creating a String
+        String greeting = "Hello";
+        //Concatinating a String
+        String sentence = greeting +", World";
+        // Using a method from the string class
+        int length = sentence.length();
+        System.out.println(sentence);
+        System.out.println(length);
+
+    }
+}
