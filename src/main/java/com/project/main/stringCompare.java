0aa7d7a034d416bb9ec1497d71cf264c5474ff04
Mourya1123 <mourya.dv1@gmail.com>
2026-01-15 15:15:12 +0530

    String Comparrision, startsWith and endsWith Methods.

---
+++ src/main/java/com/project/main/stringCompare.java
@@ -0,0 +1,14 @@
+package com.project.main;
+
+public class stringCompare {
+    public static void main(String args[]){
+        String str = "String Compare";
+        String str2 = "unhappy";
+        System.out.println(str.startsWith("String"));
+        System.out.println(str.endsWith("Compare"));
+        System.out.println(str2.startsWith("un"));
+        System.out.println(str2.endsWith("happy"));
+
+    }
+}
+
